9020e7085137c1984403325f0663f98d3e55aaac
glennin-codes <glen.nyandiri@students.kyu.ac.ke>
2023-02-22 10:20:12 +0300

    cleaning

--- src/util/UrlThumbnail.js
+++ src/util/UrlThumbnail.js
@@ -1,9 +1,9 @@
 
-import { imageUrlBuilder } from '@sanity/image-url'
-import React from 'react'
-import { API } from '../context/productContext'
+// import { imageUrlBuilder } from '@sanity/image-url'
+// import React from 'react'
+// import { API } from '../context/productContext'
 
-function UrlThumbnail(src) {
-  return imageUrlBuilder(API).image(src).width(300).url()
-}
-export {UrlThumbnail};
+// function UrlThumbnail(src) {
+//   return imageUrlBuilder(API).image(src).width(300).url()
+// }
+// export {UrlThumbnail};
