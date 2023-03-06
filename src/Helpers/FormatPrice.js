78c8cd6d3a0f8683372fa966d4fdde343c81106b
glennin-codes <glen.nyandiri@students.kyu.co.ke>
2023-03-07 00:02:26 +0300

    deploy

--- src/Helpers/FormatPrice.js
+++ src/Helpers/FormatPrice.js
@@ -1,9 +1,11 @@
 
 const FormatPrice = ({price}) => {
-
-   // Numbers over 1000 to separated by commas
-   
-    return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  if (price == null) {
+    return null; // or some default value
+  }
+  
+  // Numbers over 1000 to separated by commas
+  return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
   }
 
   
