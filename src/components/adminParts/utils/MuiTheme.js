19f9dea81836a15e9499f4a093b6b08f0b2ed959
glennin-codes <glen.nyandiri@students.kyu.ac.ke>
2023-03-24 05:46:21 +0300

    solved some bag

--- src/components/adminParts/utils/MuiTheme.js
+++ src/components/adminParts/utils/MuiTheme.js
@@ -8,7 +8,31 @@ const MuiTheme = createTheme({
     secondary: {
       main: "#f50057",
     },
+     
+
   },
-});
+  
+  typography: {
+    fontFamily: [
+      "Roboto",
+      "Helvetica",
+      "Arial",
+      "sans-serif",
+    ].join(","),
+    h1: {
+      fontSize: "2.5rem",
+      fontWeight: 600,
+      lineHeight: 1.167,
+      letterSpacing: "0em",
+    },
+       
+      fontSize: "1.25rem",
+      // add any other typography-related properties you want to apply to all components
+    }
+}
+   
+   
+
+);
 
 export default MuiTheme;
