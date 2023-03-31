f6e331fd3eb8e77fa47163ba08c5de7eb1719f4f
ayiendaglen <ayiendaglen@gmail.com>
2023-03-31 03:18:19 +0300

    fewchanges to the updateProduct   page

--- src/components/adminParts/product/UpdateProductForm.js
+++ src/components/adminParts/product/UpdateProductForm.js
@@ -1,7 +1,6 @@
-
-import React,{ useState } from "react";
-import axios from "axios";
 import {  Grid  , TextField } from "@mui/material";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import Button from "@mui/material/Button";
 import Dialog from "@mui/material/Dialog";
 import DialogTitle from "@mui/material/DialogTitle";
@@ -9,11 +8,10 @@ import DialogContent from "@mui/material/DialogContent";
 import DialogActions from "@mui/material/DialogActions";
 import Stack from '@mui/material/Stack';
 import { Box } from "@mui/system";
-import Alert from "@mui/material/Alert";
-import { ThemeProvider } from "@mui/material/styles";
-import MuiTheme from '../utils/MuiTheme';
+import Alert from "@mui/material/Alert"
+import { useNavigate } from "react-router-dom";
 
-export default function UpdateProductForm({product, onClose,setRefresh,}) {
+export default function UpdateProductForm({product, onClose,setRefresh}) {
   const [name, setName] = useState(product.name);
   const [category, setCategory] = useState(product.category);
   const [company, setCompany] = useState(product.company);
@@ -21,24 +19,30 @@ export default function UpdateProductForm({product, onClose,setRefresh,}) {
   const [stock, setStock] = useState(product.stock);
   const [open, setOpen] = useState(false);
   const[success,setSuccess]=useState("");
+  const[error,setError]=useState("");
+  const navigate=useNavigate();
   console.log(product);
   console.log(product._id)
-  const cancle=()=>{
-      setRefresh(prevState => prevState)
-   
-  }
 
   const handleSubmit = async (event) => {
 
     event.preventDefault();
     try {
-    const res = await axios.patch(`https://shopifybackend.onrender.com/api/product/${product._id}`, {
+    // Get token from local storage
+    const token = localStorage.getItem('token')
+
+      const config = {
+        headers: { Authorization: `Bearer ${token}` }
+      };
+    const res = await axios.patch(`https://comradesbizapi.azurewebsites.net/api/product/${product._id}`, {
         name,
         category,
         company,
         price,
         stock,
-      });
+      },
+      config
+      );
       if (res.status === 200) {
         setSuccess("Product updated successfully");
            setRefresh(prevState => !prevState)
@@ -48,19 +52,37 @@ export default function UpdateProductForm({product, onClose,setRefresh,}) {
       }
      
     } catch (error) {
-     
-      alert("There was an error");
+      if (error.response && error.response.status === 404) {
+        setError("Product not found");
+      
+      } 
+       else if ( error.response && error.response.status === 401) {
+        setError('You are not authorized to access this resource.');
+      }
+       else if (error.response && error.response.status === 403) {
+        setError('Access to this resource is forbidden. Please log in to continue.');
+        setTimeout(()=>{
+          navigate('/');
+        }
+        ,3000
+        )
+      }else if (error.response && error.response.status === 500){
+          console.log(error.response.data);
+         setError("Server error!");
+      }
+      else {
+        setError("network error!,check your connections and try again");
+       
+      }
       setOpen(false);
     }
   };
 
   return (
-
-    <ThemeProvider theme={MuiTheme}>
     <div>
         {success && <Alert severity='success'>{success}</Alert>}
+        {error && <Alert severity='error'>{error}</Alert>}
       <Button onClick={() => setOpen(true)}>Update Product</Button>
-      <Button onClick={cancle()}>Cancle</Button>
     
       <Box maxWidth="sm" sx={{ my: 4, mx: "auto" }}>
       <Dialog open={open} onClose={() => setOpen(false)}>
@@ -99,12 +121,16 @@ export default function UpdateProductForm({product, onClose,setRefresh,}) {
           
         </DialogContent>
         <DialogActions>
-          <Button onClick={() => setOpen(false)}>Cancel</Button>
+          <Button onClick={() => {
+          setOpen(false)
+             setRefresh(prevState => !prevState);
+             onClose()
+          }
+          }>Cancel</Button>
           <Button onClick={handleSubmit}>Update</Button>
         </DialogActions>
       </Dialog>
         </Box>
     </div>
-    </ThemeProvider>
   );
 }
