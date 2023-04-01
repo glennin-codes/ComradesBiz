18c6908d3b99f965365a1103a9855d88d24ddbd8
glennin-codes <glen.nyandiri@students.kyu.ac.ke>
2023-04-01 08:09:16 +0300

    solve error

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,13 @@
-import React, { useState } from 'react';
-import { Link, NavLink } from 'react-router-dom';
-import { Nav, Navbar, Container, Offcanvas } from 'react-bootstrap'
+import React, { useState } from "react";
+import { Link, NavLink } from "react-router-dom";
+import { Nav, Navbar, Container, Offcanvas } from "react-bootstrap";
 
 // import Logo from '../../assets/images/logo.png'
-import Logo from '../../ContactPage/assets/images/logo.png'
-import "./Header.css"
-import { HumBurgerIcon } from './Icon/Icon';
+import Logo from "../../ContactPage/assets/images/logo.png";
+import "./Header.css";
+import { HumBurgerIcon } from "./Icon/Icon";
 import { FiShoppingCart } from "react-icons/fi";
-import { useCartContext } from '../../context/cart_context';
+import { useCartContext } from "../../context/cart_context";
 
 const Header = () => {
   const { total_item } = useCartContext();
@@ -15,86 +15,102 @@ const Header = () => {
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
-
   return (
     <>
-      <div className='main_container_contact'>
-        <div className='main_container_navbar'>
+      <div className="main_container_contact">
+        <div className="main_container_navbar">
           <Navbar expand="lg" sticky="top" className="navbar">
             <Container>
               <Link to="/">
-                <img src={Logo} alt="logo" width="130px" height="130px" className="nav-img" />
+                <img
+                  src={Logo}
+                  alt="logo"
+                  width="130px"
+                  height="130px"
+                  className="nav-img"
+                />
               </Link>
 
-              <button onClick={handleShow} className="mobile__navbar"  >
+              <button onClick={handleShow} className="mobile__navbar">
                 <HumBurgerIcon onClick={handleShow} className="me-2" />
               </button>
 
               <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="me-auto NavLink-all">
-                  <Link to="/" className='NavLink' >Home</Link>
-                  <Link to="/products" className='NavLink'>Products On Sale</Link>
-                  <Link to="/about" className='NavLink'>About Us </Link>
-                  <Link to="/contact" className='NavLink'>Contact Us</Link>
-                  <Link to="/login" className='NavLink'>Profile</Link>
-                  
-            <NavLink to="/cart" className="NavLink " style={{position:'relative'}}>
-              <FiShoppingCart className="cart-trolley"  style={{fontSize : '3.2rem'}}/>
-              <span className="cart-total--item" > {total_item} </span>
-            </NavLink>
-          
-        
+                  <Link to="/" className="NavLink">
+                    Home
+                  </Link>
+                  <Link to="/products" className="NavLink">
+                    Products On Sale
+                  </Link>
+                  <Link to="/about" className="NavLink">
+                    About Us{" "}
+                  </Link>
+                  <Link to="/contact" className="NavLink">
+                    Contact Us
+                  </Link>
+                  <Link to="/login" className="NavLink">
+                    Profile
+                  </Link>
 
+                  <NavLink
+                    to="/cart"
+                    className="NavLink "
+                    style={{ position: "relative" }}
+                  >
+                    <FiShoppingCart
+                      className="cart-trolley"
+                      style={{ fontSize: "3.2rem" }}
+                    />
+                    <span className="cart-total--item"> {total_item} </span>
+                  </NavLink>
                 </Nav>
               </Navbar.Collapse>
             </Container>
           </Navbar>
         </div>
-
       </div>
-      <Offcanvas show={show}
+      <Offcanvas
+        show={show}
         onHide={handleClose}
         placement="end"
         name="end"
         className="mobile_version_offcanvas_nav"
       >
         <Offcanvas.Header closeButton>
-          <Offcanvas.Title className="mobile_version_offcanvas_nav-title">Comradesbiz</Offcanvas.Title>
+          <Offcanvas.Title className="mobile_version_offcanvas_nav-title">
+            Comradesbiz
+          </Offcanvas.Title>
         </Offcanvas.Header>
         <Offcanvas.Body>
-
-
-          <div className='mobile_version_links'>
-            <Link to="/" className='mobile_navlink' >Home</Link>
-            <Link to="/products" className='mobile_navlink'>Product On Sale</Link>
-            <Link to="/about" className='mobile_navlink'>About Us</Link>
-            <Link to="/contact" className='mobile_navlink'>Contact Us</Link>
-            <Link to="/login" className='mobile_navlink'>Profile</Link>
+          <div className="mobile_version_links">
+            <Link to="/" className="mobile_navlink">
+              Home
+            </Link>
+            <Link to="/products" className="mobile_navlink">
+              Product On Sale
+            </Link>
+            <Link to="/about" className="mobile_navlink">
+              About Us
+            </Link>
+            <Link to="/contact" className="mobile_navlink">
+              Contact Us
+            </Link>
+            <Link to="/login" className="mobile_navlink">
+              Profile
+            </Link>
             <NavLink to="/cart" className="mobile_navlink">
-              <FiShoppingCart className="cart-trolley"  />
+              <FiShoppingCart className="cart-trolley" />
               <span className="cart-total--item"> {total_item} </span>
             </NavLink>
-          
           </div>
-
-
-
         </Offcanvas.Body>
       </Offcanvas>
     </>
-  )
-}
+  );
+};
 
-export default Header
-
-
-
-
-
-
-
-
-
+export default Header;
 
 // import React from "react";
 // import { NavLink } from "react-router-dom";
--- src/privateRoute/PrivateRoute.js
+++ src/privateRoute/PrivateRoute.js
@@ -1,10 +1,12 @@
-import { Route, Navigate } from 'react-router-dom';
+import { Route, useNavigate, } from 'react-router-dom';
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
   const isAuthenticated = localStorage.getItem('token');
-
+const navigate=useNavigate();
   return (
-    <Route {...rest} element={isAuthenticated ? <Component /> : <Navigate to="/login" />} />
+     
+    
+    <Route {...rest} element={isAuthenticated ? <Component /> : navigate('/login')} />
   );
 };
 
