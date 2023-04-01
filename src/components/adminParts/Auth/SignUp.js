336ae43e91d65d3c44542a245b9d52893c220e51
glennin-codes <glen.nyandiri@students.kyu.ac.ke>
2023-04-02 02:00:22 +0300

    fixing error

--- src/components/adminParts/Auth/SignUp.js
+++ src/components/adminParts/Auth/SignUp.js
@@ -397,7 +397,7 @@ const SignUp = () => {
               </ListSubheader>
               {placeData.map(({ id, center, place_name, geometry }) => (
                 <MenuItem key={center} value={[place_name, center]}
-                style={{color:'black'}}
+                style={{color:'#01010133'}}
                 >
                   {place_name}
                 </MenuItem>
