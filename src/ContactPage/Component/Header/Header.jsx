9e455fed08170946dec9736758bf3500dd61eb54
glennin-codes <glen.nyandiri@students.kyu.ac.ke>
2023-04-01 05:09:38 +0300

    solved bug

--- src/ContactPage/Component/Header/Header.jsx
+++ src/ContactPage/Component/Header/Header.jsx
@@ -12,12 +12,17 @@ const Header = () => {
 
   return (
      <>
+
+<div className='main_container_contact'>
+        <div className='main_container_navbar'>
         <div className='main_container_contact_content'>
           <div className='main_container_contact_content-subHeading'>
             <h3>CONTACT US</h3>
             <h1 style={{color:'white'}}>Talk To The<span className='main_container_contact_content-subHeading-span'>Advisory Product  Managers </span></h1>
           </div>
         </div>
+        </div>
+      </div>
 </>
      
   )
