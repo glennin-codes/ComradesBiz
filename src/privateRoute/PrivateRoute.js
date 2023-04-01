41305ec0547c3860a1fd49f92f6e9c137e72c8d6
glennin-codes <glen.nyandiri@students.kyu.ac.ke>
2023-04-01 07:56:44 +0300

    clean

--- src/privateRoute/PrivateRoute.js
+++ src/privateRoute/PrivateRoute.js
@@ -1,14 +1,10 @@
-import { Route, Redirect } from 'react-router-dom';
+import { Route, Navigate } from 'react-router-dom';
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
   const isAuthenticated = localStorage.getItem('token');
 
   return (
-    <Route {...rest} render={(props) => (
-      isAuthenticated
-        ? <Component {...props} />
-        : <Redirect to="/login" />
-    )} />
+    <Route {...rest} element={isAuthenticated ? <Component /> : <Navigate to="/login" />} />
   );
 };
 
