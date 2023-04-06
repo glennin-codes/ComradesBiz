baf5196d426f6be0d66e1f646517c2ee9eb0ffde
glennin-codes <glen.nyandiri@students.kyu.ac.ke>
2023-04-06 17:11:23 +0300

    maintainance isues

--- src/components/adminParts/utils/Maintanance/Maintanance.js
+++ src/components/adminParts/utils/Maintanance/Maintanance.js
@@ -1,16 +1,16 @@
 import React, { useState } from "react";
-import './MaintananceMessage.css'
+import "./MaintananceMessage.css";
 
 const MaintenanceMessage = () => {
-  const [position, setPosition] = useState(100); // initial position is 100 pixels from the right
+  const [position, setPosition] = useState("50%"); // initial position is centered
 
   return (
     <div className="maintenance-message">
-      <span className="text">
+      <span className="text" >
         Our system is currently under maintenance to resolve some minor issues. We apologize for any inconvenience caused and appreciate your patience during this time.
       </span>
     </div>
   );
-}
+};
 
 export default MaintenanceMessage;
