5c8adede8af8581c7ca9b3153b551cfd725cf5cf
glennin-codes <glen.nyandiri@students.kyu.ac.ke>
2023-05-02 15:35:56 +0300

    socila flow solved error

--- src/Socials/SocialFlow.js
+++ src/Socials/SocialFlow.js
@@ -1,12 +1,6 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import "./socialFlow.css";
-import { FaFacebook, FaInstagram, FaTwitter, FaWhatsapp } from "react-icons/fa";
-// import {
-//   faFacebook,
-//   faTwitter,
-//   faWhatsapp,
-//   faInstagram
-// } from "@fortawesome/free-brands-svg-icons";
+import { Box, IconButton, Link, List, ListItem, ListItemIcon, ListItemText } from '@mui/material';
+import { WhatsApp, Facebook, Instagram } from '@mui/icons-material';
+
 
 export default function SocialFlow({infos}) {
   const {phone,name,price,image}=infos
@@ -14,59 +8,43 @@ export default function SocialFlow({infos}) {
 const message = `Hi, I'm interested in your ${name} product that is priced at ${price}. Can you provide me with more information?<br><img src="${image[0]?.url}" alt="${name}">`;
 
 const whatsappLink = `https://api.whatsapp.com/send?phone=${phone}&text=${encodeURIComponent(message)}`;
+  const facebookLink = `https://www.facebook.com/sharer.php?u=${encodeURIComponent(window.location.href)}`;
+  const instagramLink = `https://www.instagram.com/sharer.php?u=${encodeURIComponent(window.location.href)}`;
 
   return (
-    <nav>
-      <ul>
-        <li>
-          <a
-            href="https://www.facebook.com/profile.php?id=100091573156387"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <i>
-              <FontAwesomeIcon icon={FaFacebook} className="icon" />
-            </i>
-          
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://twitter.com/@comradesbiz"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <i>
-              <FontAwesomeIcon icon={FaTwitter} className="icon" />
-            </i>
-            
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://www.instagram.com/comradesbiz/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <i>
-              <FontAwesomeIcon icon={FaInstagram} className="icon" />
-            </i>
-            
-          </a>
-        </li>
-        <li>
-          <a
-            href={whatsappLink}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <i>
-              <FontAwesomeIcon icon={FaWhatsapp} className="icon" />
-            </i>
-            
-          </a>
-        </li>
-      </ul>
-    </nav>
+    <Box
+      sx={{
+        position: 'fixed',
+        bottom: '20px',
+        right: '20px',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <List sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', gap: '10px' }}>
+        <ListItem disablePadding>
+          <ListItemIcon>
+            <IconButton component={Link} href={whatsappLink} target="_blank" rel="noopener noreferrer">
+              <WhatsApp />
+            </IconButton>
+          </ListItemIcon>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemIcon>
+            <IconButton component={Link} href={facebookLink} target="_blank" rel="noopener noreferrer">
+              <Facebook />
+            </IconButton>
+          </ListItemIcon>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemIcon>
+            <IconButton component={Link} href={instagramLink} target="_blank" rel="noopener noreferrer">
+              <Instagram />
+            </IconButton>
+          </ListItemIcon>
+        </ListItem>
+      </List>
+    </Box>
   );
-}
+};
